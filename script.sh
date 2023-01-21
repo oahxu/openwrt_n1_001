f934768aa19b62e67766f8a5199c5582489f591a
oahxu <oahxu@users.noreply.github.com>
2023-01-21 21:08:56 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd openwrt
+# Add luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
+mv -f package-temp/luci-app-adguardhome package/lean/
+rm -rf package-temp
+
+# Add luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package-temp
+mv -f package-temp/luci-app-openclash package/lean/
+rm -rf package-temp
+# Add luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
+rm -rf theme-temp/luci-theme-opentomcat/LICENSE
+rm -rf theme-temp/luci-theme-opentomcat/README.md
+mv -f theme-temp/luci-theme-opentomcat package/lean/
+rm -rf theme-temp
+default_theme='opentomcat'
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+# Add luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/kenzok8/small.git package-temp/small
+cp -r package-temp/small/* package/lean/
+mv -f package-temp/lua-maxminddb package/lean/
+mv -f package-temp/luci-app-vssr package/lean/
+rm -rf package-temp
